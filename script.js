89f42244cc130b472f737dd52a42ae8f489986b3
gabb <gabriela_b_ferreira@estudante.sesisenai.org.br>
2026-03-14 20:00:10 -0300

    add- adicionei a resolução do exercício 2

--- script.js
+++ script.js
@@ -1,12 +1,18 @@
+console.log("Teste funcionando");
+
 
 function atividade1() {
+
 
     let total = 0;
     let continuar = true;
 
+
     while (continuar) {
 
+
         let valor = Number(prompt("Informe um número (0 para parar):"));
+
 
         if (isNaN(valor)) {
             alert("Digite um valor numérico válido.");
@@ -14,12 +20,32 @@ function atividade1() {
             total = total + valor;
         }
 
+
         continuar = confirm("Deseja inserir outro número?");
     }
+
 
     alert("Resultado da soma: " + total);
     console.log("Resultado da soma:", total);
 }
 //atividade1()
+
+function atividade2() {
+
+
+    console.log("Preparando lançamento...");
+
+
+    for (let contador = 10; contador >= 0; contador--) {
+        console.log(contador);
+    }
+
+
+    console.log("Foguete lançado com sucesso!");
+}
+//atividade2()
+
+
+
 
 
